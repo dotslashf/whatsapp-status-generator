02ce9b0f53a90bf192a3193bbb643923c5a427b2
dotslashf <fadhlurahman.i@gmail.com>
2023-11-15 21:42:51 +0800

    feat: add social media meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,16 +26,40 @@ export default function RootLayout({
       lang="en"
       className={`w-full ${dosis.variable} ${inconsolata.variable}`}
     >
-      <Script src='"https://www.googletagmanager.com/gtag/js?id=G-W5DVFW6FMZ"' />
-      <Script id="google-analytics">
-        {`
+      <head>
+        <Script src='"https://www.googletagmanager.com/gtag/js?id=G-W5DVFW6FMZ"' />
+        <Script id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
- 
+          
           gtag('config', 'G-W5DVFW6FMZ');
-        `}
-      </Script>
+          `}
+        </Script>
+        <meta
+          property="twitter:image"
+          content="Twitter link preview image URL"
+        ></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta
+          property="twitter:title"
+          content="Whatsapp Status Generator"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://dynamic-og-image-generator.vercel.app/api/generate?title=Whatsapp+Status+Generator&author=&websiteUrl=https%3A%2F%2Fwhatsapp-status-generator.vercel.app%2F&avatar=&theme=default"
+        ></meta>
+        <meta
+          name="twitter:image"
+          content="https://dynamic-og-image-generator.vercel.app/api/generate?title=Whatsapp+Status+Generator&author=&websiteUrl=https%3A%2F%2Fwhatsapp-status-generator.vercel.app%2F&avatar=&theme=default"
+        />
+        <meta
+          property="og:url"
+          content="https://whatsapp-status-generator.vercel.app/"
+        ></meta>
+        <meta property="og:title" content="Whatsapp Status Generator"></meta>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

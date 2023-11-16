bb941925a2c0543ffbc4f18064fb62ff67d0379e
dotslashf <fadhlurahman.i@gmail.com>
2023-11-16 10:16:08 +0800

    fix: Google Analytics configuration in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_GTAG}'});
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_GTAG}');
           `}
       </Script>
       <head>
